7b1d2642c7ab7b26424a068a32ee06d5c8dea817
Jinghuan Yu <supertempler@gmail.com>
2023-03-14 20:25:11 -0700

    update workload e

--- tools/seek-and-insert/running_after_migrating.sh
+++ tools/seek-and-insert/running_after_migrating.sh
@@ -12,7 +12,7 @@ redis-cli -h 127.0.0.1 -p 40001 stats > rocks_stat/$1/separate/before_run.node1.
 redis-cli -h 127.0.0.1 -p 40002 stats > rocks_stat/$1/separate/before_run.node2.stat
 redis-cli -h 127.0.0.1 -p 40003 stats > rocks_stat/$1/separate/before_run.node3.stat
 
-../ycsb/ycsb -run -db redis -P $WORKLOAD -P ../ycsb/cluster.prop -s -threads 6
+../../../YCSB-cpp-Redis/ycsb -run -db redis -P $WORKLOAD -P ../ycsb/cluster.prop -s -threads 6
 
 redis-cli -h 127.0.0.1 -p 40001 stats > rocks_stat/$1/separate/after_run.node1.stat
 redis-cli -h 127.0.0.1 -p 40002 stats > rocks_stat/$1/separate/after_run.node2.stat
