bc50dc22c766ba57393700bae4b7253120110971
Jinghuan Yu <supertempler@gmail.com>
2023-05-17 05:38:21 -0700

    add the result of scale up baselines

---
+++ tools/section2-scale-up/cluster_restart.sh
@@ -0,0 +1,17 @@
+./create_cluster.sh stop
+./create_cluster.sh start
+./create_cluster.sh create
+
+echo "Server created"
+./loading.sh m
+echo "Data loaded"
+./migrate.sh 2
+#./running_while_migrating.sh a 0
+#redis-cli -c -p 40001 clusterx migrate 2410 2518  kvrockskvrockskvrockskvrockskvrocksnode2
+#redis-cli -p 40001 clusterx setslot 2515 node kvrockskvrockskvrockskvrockskvrocksnode2 3
+#redis-cli -p 40002 clusterx setslot 2515 node kvrockskvrockskvrockskvrockskvrocksnode2 3
+
+echo "40002 test"
+redis-cli -c -p 40002 HMSET hello{hash_tag} field1 "Hello" field2 "World"
+echo "40001 test"
+redis-cli -c -p 40001 HMSET hello{hash_tag} field1 "Hello" field2 "World"
