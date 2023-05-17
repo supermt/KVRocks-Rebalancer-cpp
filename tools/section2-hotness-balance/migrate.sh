fef690045bda8a26da5894b31beba37896c2e950
Jinghuan Yu <supertempler@gmail.com>
2023-05-17 05:39:42 -0700

    add the hotness case test

---
+++ tools/section2-hotness-balance/migrate.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+echo "Start Migration"
+echo $1
+export PYTHONPATH=$PYTHONPATH:../../
+
+redis-cli -h 127.0.0.1 -p 40001 stats > ./before_migration.node1.stat
+redis-cli -h 127.0.0.1 -p 40002 stats > ./before_migration.node2.stat
+
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>>>Add Server<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+#python3 ../../case_scaleup.py
+
+#sleep 10
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>Load Balance<<<<<<<<<<<<<<<<<<<<<<<<<"
+python3 ../../case_hotness_balance.py $1
+
+redis-cli -h 127.0.0.1 -p 40001 stats > ./before_migration.node1.stat
+redis-cli -h 127.0.0.1 -p 40002 stats > ./before_migration.node2.stat
+
+
+
+echo "Migration Finished"
+
+# redis-cli -c -p 40001 clusterx migrate 0 8192 kvrockskvrockskvrockskvrockskvrocksnode2
+
+# redis-cli -c -p 40001 clusterx migrate 0 8192 kvrockskvrockskvrockskvrockskvrocksnode2
+
+#redis-cli -p 40001 clusterx migrate 0 8192 kvrockskvrockskvrockskvrockskvrocksnode2
