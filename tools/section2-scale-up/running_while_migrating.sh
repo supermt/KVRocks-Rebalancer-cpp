9953a4266b8501696c33ca0c16325c6cc4526675
Jinghuan Yu <supertempler@gmail.com>
2023-05-18 07:39:04 -0700

    finish motivation part, no more experiments

--- tools/section2-scale-up/running_while_migrating.sh
+++ tools/section2-scale-up/running_while_migrating.sh
@@ -3,4 +3,4 @@ export WORKLOAD="../ycsb/workload$1"
 echo $WORKLOAD
 
 nohup ./migrate.sh $2 > balance.log 
-../../../YCSB-cpp-Redis/ycsb -run -db redis -P $WORKLOAD -p status.interval=1 -P ../ycsb/cluster.prop -s -threads 6
+../../../YCSB-cpp-Redis/ycsb -run -db redis -P $WORKLOAD -p status.interval=1 -P ../ycsb/cluster.prop -s -threads 3
