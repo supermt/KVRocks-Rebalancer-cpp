e4159a3ea0e5bf0380704a719436eb1287cfd1ab
Jinghuan Yu <supertempler@gmail.com>
2023-06-22 04:33:08 -0700

    update baseline

--- tools/baseline/cluster_restart.sh
+++ tools/baseline/cluster_restart.sh
@@ -4,7 +4,8 @@
 
 echo "Server created"
 #./loading.sh i
-../../../redis/src/redis-benchmark -p 40001 -t set -n 10000000 -r 100000000 -d 1000 -q -P 1000 --cluster
+../../../redis/src/redis-benchmark -p 40001 -t hset -n 10000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 1
+#../../../redis/src/redis-benchmark -p 40001 -t set -n 10000000 -r 100000000 -d 1000 -q -P 1000 --cluster
 mv ./redis-benchamrk.result ./redis-benchamrk.result_single_node
 echo "Data loaded"
 #./running.sh i
--- tools/baseline/dual_cluster_restart.sh
+++ tools/baseline/dual_cluster_restart.sh
@@ -4,7 +4,7 @@
 
 echo "Server created"
 #./loading.sh i
-../../../redis/src/redis-benchmark -p 40001 -t set -n 10000000 -r 100000000 -d 1000 -q -P 1000  --cluster 
+../../../redis/src/redis-benchmark -p 40001 -t hset -n 40000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 2
 mv ./redis-benchamrk.result ./redis-benchamrk.result_dual_node
 echo "Data loaded"
 #./running.sh i
