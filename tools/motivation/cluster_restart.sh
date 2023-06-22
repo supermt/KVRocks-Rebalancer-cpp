4df788ec7063eeb72c00971a970fab5bf5a4e2a1
Jinghuan Yu <supertempler@gmail.com>
2023-06-22 04:09:51 -0700

    update a better motivation

--- tools/motivation/cluster_restart.sh
+++ tools/motivation/cluster_restart.sh
@@ -4,12 +4,12 @@
 
 echo "Server created"
 date +"%H:%M:%S.%s"
-../../../redis/src/redis-benchmark -p 40001 -t hset -n 1000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 2
-mv ./redis-benchamrk.result ./redis-benchamrk.result_dual_node
+../../../redis/src/redis-benchmark -p 40001 -t hset -n 1000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 1
+mv ./redis-benchamrk.result ./redis-benchamrk.result_single_node
 #nohup ./migrate.sh 0 > balance.log & 
 ./migrate.sh 0
 date +"%H:%M:%S.%s"
 ../../../redis/src/redis-benchmark -p 40001 -t hset -n 40000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 2
 d -c 2ate +"%H:%M:%S.%s"
-mv ./redis-benchamrk.result ./redis-benchamrk.result_triple_node
+mv ./redis-benchamrk.result ./redis-benchamrk.result_double_node
 echo "Data loaded"
