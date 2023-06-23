3f88a263b20505c7383dfe4913db08e41ecf2475
Jinghuan Yu <supertempler@gmail.com>
2023-06-22 23:23:03 -0700

    update baseline

--- tools/baseline/cluster_restart.sh
+++ tools/baseline/cluster_restart.sh
@@ -4,6 +4,8 @@
 
 echo "Server created"
 #./loading.sh i
+../../../redis/src/redis-benchmark -p 40001 -t hset -n 2000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 1
+mv ./redis-benchamrk.result ./redis-benchamrk.result_single_node_warm
 ../../../redis/src/redis-benchmark -p 40001 -t hset -n 10000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 1
 #../../../redis/src/redis-benchmark -p 40001 -t set -n 10000000 -r 100000000 -d 1000 -q -P 1000 --cluster
 mv ./redis-benchamrk.result ./redis-benchamrk.result_single_node
--- tools/baseline/dual_cluster_restart.sh
+++ tools/baseline/dual_cluster_restart.sh
@@ -4,6 +4,10 @@
 
 echo "Server created"
 #./loading.sh i
+
+../../../redis/src/redis-benchmark -p 40001 -t hset -n 25531600 -r 100000000 -d 1000 -q -P 100 --cluster -c 2
+mv ./redis-benchamrk.result ./redis-benchamrk.result_double_node_warm
+
 ../../../redis/src/redis-benchmark -p 40001 -t hset -n 40000000 -r 100000000 -d 1000 -q -P 100 --cluster -c 2
 mv ./redis-benchamrk.result ./redis-benchamrk.result_dual_node
 echo "Data loaded"
